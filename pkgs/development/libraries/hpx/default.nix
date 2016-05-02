8a03b05a05d35c8be645ee8826cf5e3c7cb8f560
Ryan Newton <rrnewton@gmail.com>
2016-05-02 17:15:52 -0400

    fix to previous

--- pkgs/development/libraries/hpx/default.nix
+++ pkgs/development/libraries/hpx/default.nix
@@ -23,7 +23,7 @@ in stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ stdenv ];
 
-  configureFlags = ["-g", "--enable-shared"]
+  configureFlags = [ "-g" "--enable-shared" ]
     ++ optional enableDebug "--enable-debug"
     ;
       
