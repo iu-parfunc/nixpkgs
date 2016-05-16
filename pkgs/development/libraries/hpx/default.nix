17781a02a973609642e7898299390e5db155649d
Ryan Newton <rrnewton@gmail.com>
2016-05-16 13:13:06 -0400

    hpx: init at 2.2.0
    Squashed commit of the following:
    
    commit cf6899e67097f5ca41d510706e4ffc249620c3bc
    Author: Ryan Scott <ryan.gl.scott@gmail.com>
    Date:   Mon May 2 14:57:19 2016 -0400
    
        Make the url more tolerant to change
    
    commit f1fe9068d462ffc24b1170070dde15b502c191a6
    Author: Ryan Newton <rrnewton@gmail.com>
    Date:   Mon May 2 17:18:42 2016 -0400
    
        Urgh, another fix
    
    commit 8a03b05a05d35c8be645ee8826cf5e3c7cb8f560
    Author: Ryan Newton <rrnewton@gmail.com>
    Date:   Mon May 2 17:15:52 2016 -0400
    
        fix to previous
    
    commit 6fbd63b36fa58b96a7d60255ce64864e7f7e94d6
    Author: Ryan Newton <rrnewton@gmail.com>
    Date:   Mon May 2 17:13:24 2016 -0400
    
        Enable shared and symbols for hpx.
    
    commit 8d578460f9bdaeee736ac4bbda75e7bbcd0be88e
    Author: Ryan Newton <rrnewton@gmail.com>
    Date:   Mon May 2 14:47:21 2016 -0400
    
        hpx: Remove dependence on builder script
    
    commit 088b136109c1f5dbaef0e2243fbe05656d5fb96e
    Author: Ryan Newton <rrnewton@gmail.com>
    Date:   Mon May 2 14:25:31 2016 -0400
    
        Add hpx-2.2.0: a distributed-programming runtime system

---
+++ pkgs/development/libraries/hpx/default.nix
@@ -0,0 +1,38 @@
+{stdenv, fetchurl, perl
+# Turn on debugging
+, enableDebug ? false
+}:
+
+with stdenv.lib;
+
+let hpxVersion = "2.2.0";
+
+in stdenv.mkDerivation rec {       
+  name = "hpx-${hpxVersion}";
+
+  src = fetchurl {
+    url = "http://hpx.crest.iu.edu/release/${name}.tar.gz";
+    sha256 = "01sc85ifwdg87jvcgkkqmjg09j2h1d9i1y5vb3lifhrq6za399bs";
+  };
+
+  enableParallelBuilding = true;
+  
+  preConfigure = ''
+    cd hpx/
+  '';
+
+  nativeBuildInputs = [ stdenv ];
+
+  configureFlags = [ "CFLAGS=-g" "--enable-shared" ]
+    ++ optional enableDebug "--enable-debug"
+    ;
+      
+  meta = {
+    homepage = https://hpx.crest.iu.edu/;
+    description = "Open source High Performance paralleX implementation";
+    longDescription = "HPX-5 (High Performance ParalleX) is an open source, portable, performance-oriented runtime developed at CREST (Indiana University). HPX-5 provides a distributed programming model allowing programs to run unmodified on systems from a single SMP to large clusters and supercomputers with thousands of nodes.
+HPX-5 supports a wide variety of Intel and ARM platforms. It is being used by a broad range of scientific applications enabling scientists to write code that performs and scales better than contemporary runtimes.";
+  };
+  
+}
+
